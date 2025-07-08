bee5e2b2d42d85a319f7c85a48b9a224388bbad6
Samir Zaman <samir.zaman40@gmail.com>
2025-07-08 13:32:19 -0700

    Q1 attempt 1

---
+++ gfe1.js
@@ -0,0 +1,14 @@
+//Debounce
+
+export default function debounce(func, wait) {
+    let timeoutId
+
+    return function (...args) {
+        const context = this; //preserve context
+        clearTimeout(timeoutId); //clears previous timer if debounce is invoked a second time
+
+        setTimeout(() => {
+            func(...args);
+        }, wait);
+    };
+}
